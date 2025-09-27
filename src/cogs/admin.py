7140505ac77f26f96225d0275c15d55d12b1adcc
Konyári László <konyarilaszlo25@gmail.com>
2025-09-27 12:40:24 +0200

    using sqlite3 to store the admins

--- src/cogs/admin.py
+++ src/cogs/admin.py
@@ -1,6 +1,12 @@
 from discord import app_commands
 from discord.ext import commands
 import discord
+import sqlite3
+
+connection = sqlite3.connect("botdatabase.db")
+cursor = connection.cursor()
+
+cursor.execute("CREATE TABLE IF NOT EXISTS admins (guild_id INTEGER, user_id INTEGER)")
 
 class Admin(commands.Cog):
     def __init__(self, bot):
@@ -21,17 +27,47 @@ class Admin(commands.Cog):
     async def add(self, interaction: discord.Interaction, user: discord.User):
         if interaction.user != interaction.guild.owner:
             await interaction.response.send_message("Only the owner can add other admins!", ephemeral=True)
+            return
         else:
+            guild_id = interaction.guild.id
             self.admins.add(user.id)
+            cursor.execute("""INSERT INTO admins (guild_id, user_id) VALUES (?, ?) """, (guild_id, user.id))
+            connection.commit()
             await interaction.response.send_message(f"{user.mention} is now an admin! 🥳")
 
     @admin.command(name="remove")
     async def add(self, interaction: discord.Interaction, user: discord.User):
         if interaction.user != interaction.guild.owner:
             await interaction.response.send_message("Only the owner can remove other admins!", ephemeral=True)
+            return
         else:
             self.admins.remove(user.id)
             await interaction.response.send_message(f"{user.mention} is no longer an admin! 👌")
+
+    @admin.command(name="admins")
+    async def admins(self, interaction: discord.Interaction):
+        if interaction.user != interaction.guild.owner:
+            await interaction.response.send_message("Only the owner can list other admins!", ephemeral=True)
+            return
+        else:
+            guild_id = interaction.guild.id
+            cursor.execute("SELECT user_id FROM admins WHERE guild_id = ?", (guild_id,))
+            admin_ids = [row[0] for row in cursor.fetchall()]
+
+            if len(admin_ids) == 0:
+                await interaction.response.send_message("No admins added yet.", ephemeral=True)
+                return
+
+            mentions = []
+            for uid in admin_ids:
+                member = interaction.guild.get_member(uid)
+                if member:
+                    mentions.append(member.mention)
+                else:
+                    mentions.append(f"<@{uid}>")
+
+            await interaction.response.send_message("List of admins: " + ", ".join(mentions), ephemeral=True)
+
 
 
 async def setup(bot: commands.Bot):
--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 from dotenv import load_dotenv
+import sqlite3
 
 load_dotenv()
 
