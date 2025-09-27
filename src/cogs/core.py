cea3f40828016adefe8ccbbf24ec58f41cb751ed
Konyári László <konyarilaszlo25@gmail.com>
2025-09-27 10:28:27 +0200

    Added base files of the bot

---
+++ src/cogs/core.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+import os
+import logging
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+class Core(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @app_commands.command(name="ping", description="Is the bot alive?!")
+    async def ping(self, interaction: discord.Interaction):
+        await interaction.response.send_message("Pong!")
+
+    @app_commands.command(name="echo", description="Echoes back your message")
+    async def echo(self, interaction: discord.Interaction, text: str):
+        await interaction.response.send_message(text)
+
+    @app_commands.command(name="help", description="List of the bot's commands")
+    async def help(self, interaction: discord.Interaction):
+        await interaction.response.send_message(
+            "`/ping` Checks if the bot is alive or not\n`/echo` Sends you back the same text you provided as parameter"
+        )
+
+class EventsCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        ch = member.guild.system_channel
+        if ch:
+            await ch.send(f"Welcome to the server {member.mention}!")
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        ch = member.guild.system_channel
+        if ch:
+            await ch.send(f"{member.mention} has left :(")
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        if message.author.bot:
+            return
+        if message.content == "test":
+            await message.channel.send("Command worked")
+
+
+
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(Core(bot))
+    await bot.add_cog(EventsCog(bot))
---
+++ src/main.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+import os
+import logging
+import discord
+from discord import app_commands
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+GUILD = os.getenv("GUILD")
+
+class Bot(commands.Bot):
+    def __init__(self):
+        intents = discord.Intents.all()
+        intents.guilds = True
+        activity = discord.Game(name="/help")
+        super().__init__(command_prefix=commands.when_mentioned_or("!"), intents=intents, activity=activity)
+
+
+
+    async def setup_hook(self) -> None:
+        await self.load_extension("cogs.core")
+        #await self.load_extension("cogs.fun")
+
+        if GUILD and GUILD.isdigit():
+            guild = discord.Object(id=int(GUILD))
+            self.tree.copy_global_to(guild=guild)
+            await self.tree.sync(guild=guild)
+
+            logging.info("Synced commands to GUILD")
+        else:
+            await self.tree.sync()
+            logging.info("Synced global commands")
+
+    async def on_ready(self):
+        logging.basicConfig(level=logging.INFO)
+        logging.info("Logged is as ModMeister")
+        print("Logged is as ModMeister")
+
+
+
+if __name__ == "__main__":
+    if not TOKEN:
+        raise SystemExit("Set DISCORD_TOKEN in your .env file first!")
+    bot = Bot()
+    bot.run(TOKEN)
