ff99cc1f583eefbc6afe23d00ac47f05c75de366
Konyári László <konyarilaszlo25@gmail.com>
2025-09-28 13:04:18 +0200

    Expanded /help command with the admin commands

--- src/cogs/core.py
+++ src/cogs/core.py
@@ -22,7 +22,7 @@ class Core(commands.Cog):
     @app_commands.command(name="help", description="List of the bot's commands")
     async def help(self, interaction: discord.Interaction):
         await interaction.response.send_message(
-            "`/ping` Checks if the bot is alive or not\n`/echo` Sends you back the same text you provided as parameter"
+            """`/ping` Checks if the bot is alive or not\n`/echo` Sends you back the same text you provided as parameter\n`/admin add` Add admin role to user\n`/admin remove` Remove admin role from user\n`/admin ban` Ban user\n`/admin list` List of admins"""
         )
 
 class EventsCog(commands.Cog):
