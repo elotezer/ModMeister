f318d04a0a4a0a0dd1cacd847574e4f3cffbb7aa
Konyári László <konyarilaszlo25@gmail.com>
2026-04-16 12:10:05 +0200

    updated start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-cd ~/ModMeister
-pm2 start src/main.py --interpreter ./venv/bin/python3 --name "modmeister-bot"
+set -e
+
+cd ~/ModMeister || exit
+
+echo "--- ModMeister Bot indítása ---"
+pm2 start modmeister-bot || pm2 start src/main.py --interpreter ./venv/bin/python3 --name "modmeister-bot"
 pm2 save
+
+echo "--- Bot elindítva! ---"
