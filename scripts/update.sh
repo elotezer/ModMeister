6a5d2c9a93286cfd5db14c47b3e51af0542e7ae2
Konyári László <konyarilaszlo25@gmail.com>
2026-04-16 12:11:27 +0200

    enhanced the update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
+set -e
+
+cd ~/ModMeister || exit
 
 echo "--- Frissítés indítása: $(date) ---"
 
-git pull origin master
+echo "Kód letöltése..."
+git pull
 
+echo "Függőségek frissítése..."
 ./venv/bin/pip install -r requirements.txt
 
-pm2 start modmeister-bot
+echo "Bot újraindítása..."
+pm2 restart modmeister-bot
 
 echo "--- Frissítés sikeresen befejeződött! ---"
 echo "Logok megtekintése: pm2 logs modmeister-bot"
